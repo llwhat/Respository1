6d2a874e084d1267087f24ec43a4f6b3d56868e8
llwhat <91524700+llwhat@users.noreply.github.com>
2021-11-29 22:58:57 +0800

    Update numThreadInProcess.c

--- numThreadInProcess.c
+++ numThreadInProcess.c
@@ -15,19 +15,19 @@ void* thread_func(void* arg)
 int main()
 {
 
-  i=0,s=0;
+  int i=0,s=0;
   while(1)
   {
     THREAD_INFO tinfo;
     tinfo.num=i++;
     s=pthread_create(&tinfo.tid,0,thread_fucn,0);
     if(s!=0){
-    printf("fail create num %d,quit!\n", tinfo.tid);
-    exit(1);
-  }
-  else{
-    printf("tid=%d\t\tnum=%d\n",tinfo.tid,tinfo.num);
-  }
-    
+      printf("fail create num %d,quit!\n", tinfo.tid);
+      exit(1);
+    }
+    else{
+      printf("tid=%d\t\tnum=%d\n",tinfo.tid,tinfo.num);
+    }
+  }  
   return 0;
 }
